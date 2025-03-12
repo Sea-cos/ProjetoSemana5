0db31acf720aba8fbecc125d5c80e9e38b847b3b
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-03-12 19:51:41 -0300

    feat: adicionado atributos e métodos iniciais da classe RelatorioImpacto

--- src/RelatorioImpacto.java
+++ src/RelatorioImpacto.java
@@ -1,2 +1,25 @@
 public class RelatorioImpacto {
+
+    private int arvoresPlantadas;
+    private double reducaoCO2;
+
+    public int getArvoresPlantadas() {
+        return arvoresPlantadas;
+    }
+
+    public void setArvoresPlantadas(int arvoresPlantadas) {
+        this.arvoresPlantadas = arvoresPlantadas;
+    }
+
+    public double getReducaoCO2() {
+        return reducaoCO2;
+    }
+
+    public void setReducaoCO2(double reducaoCO2) {
+        this.reducaoCO2 = reducaoCO2;
+    }
+
+    public String exibirRelatorio(){
+        return "";
+    }
 }
